165cc0fd5d93ca802a5e045412d859de98f50613
Arpit technical trainer <arpit.bhawarkuacampus@gmail.com>
2025-04-29 19:57:44 +0530

    new29
    new29

---
+++ 7-8(MERN)/NODE_JS/login.js
@@ -0,0 +1,13 @@
+function login(name,pws){
+    
+    if(name==="rohan" && pws===123456){
+        console.log("login Successfully");
+    }else{
+        console.log("invalid username or password")
+    }
+}
+
+login("rohan",12345);
+
+
+
